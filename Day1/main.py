44db2ca4f07bf1ca1c60852410f17e368226cb9f
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2022-12-11 13:25:28 +0200

    day1

--- Day1/main.py
+++ Day1/main.py
@@ -5,6 +5,8 @@ input = f.readlines()
 the_highest_calories = 0
 current_calories = 0
 
+total_calories_list = []
+
 for line in input:
     if line != "\n":
         current_calories += int(line)
@@ -12,9 +14,16 @@ for line in input:
         if current_calories > the_highest_calories:
             the_highest_calories = current_calories
 
+        total_calories_list.append(current_calories)
         current_calories = 0
 
-print(f"the_most_calories: {the_highest_calories}")
+#Part 1 //////////////////////////////////////////////
 
+print(f"The most calories: {the_highest_calories}")
+
+#Part 2  //////////////////////////////////////////////
+
+sorted_total_calories_list = sorted(total_calories_list, reverse=True)
+print(f"Total calories from top3: {sum(sorted_total_calories_list[0:3])}")
 
 
