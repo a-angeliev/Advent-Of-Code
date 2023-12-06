be7cc1f63af46a5a3a736294e1e4efbecbb48d29
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-12-06 15:49:16 +0200

    updated day 6

--- 2023/Day-06/main.py
+++ 2023/Day-06/main.py
@@ -28,13 +28,14 @@ print(f"Answer part 1: {numpy.prod(ways)}")
 
 c_ways = 0
 step = 60000
-center_to_left = int(time_2[0] /2)
-center_to_right = int((time_2[0]/ 2) + 1)
+center = int(time_2[0] /2)
 c_time = time_2[0]
 c_dist = dist_2[0]
-def to_left():
+
+
+def calc():
     c_ways = 0
-    for cursor in range(center_to_left-step, 0, -step):
+    for cursor in range(center - step, 0, -step):
         travel_distance = (c_time - cursor) * cursor
         if travel_distance > c_dist:
             c_ways += step
@@ -46,23 +47,7 @@ def to_left():
             break
     return c_ways
 
-def to_right():
-    c_ways = 0
-    for cursor in range(center_to_right + step, time_2[0], step):
-        travel_distance = (c_time - cursor) * cursor
-        if travel_distance > c_dist:
-            c_ways += step
-        else:
-            for c in range(cursor - step, time_2[0]):
-                travel_distance = (c_time - c) * c
-                if travel_distance > c_dist:
-                    c_ways += 1
-            break
+c_ways += calc()
 
-    return c_ways
-
-c_ways += to_left()
-c_ways += to_right()
-
-print("Answer for part 2: ", c_ways)
+print("Answer for part 2: ", c_ways*2 - 1 )
 print(timetime.time() - start)
